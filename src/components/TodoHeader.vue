af0611eca64fbda3d09105ad0490210440aac8ee
jihyuno301 <joh755807@gmail.com>
2019-04-08 02:43:30 +0900

    TodoHeader component

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,11 +1,19 @@
 <template lang="html">
-  <div>header</div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
 export default {
 }
 </script>
-
+<!--scoped : 스타일 정의를 해당 컴포넌트에만 적용하겠다 -->
 <style lang="css" scoped>
+
+  h1 {
+    color : #2F3B52;
+    font-weight : 900;
+    margin : 2.5rem 0 1.5rem;
+  }
 </style>
